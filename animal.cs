83645bad9d24ced3da365392e821c2721d33c676
kasideshsn-commits <kasidesh.sn@cs.rmu.ac.th>
2026-01-25 22:47:27 +0700

    Update animal.cs

--- animal.cs
+++ animal.cs
@@ -1,118 +1,280 @@
 using System;
 
-enum Color {
-    BLACK, WHITE, GREY, BROWN, DARK_BROWN, BLACK_STRIPES, WHITE_STRIPES
-}
-
-enum Size {
-    SMALL, MEDIUM, LARGE, HUGE
-}
-
-interface IAnimal {
-    void Sound();
-    void Eat();
-}
-
-abstract class Animal {
-    protected Color color;
-    protected Size size;
-
-    protected Animal(Color color, Size size) {
-        this.color = color;
-        this.size = size;
+namespace AnimalSystem
+{
+    enum Color
+    {
+        BLACK, WHITE, GREY, BROWN, DARK_BROWN, BLACK_STRIPES, WHITE_STRIPES
     }
 
-    public override string ToString() {
-        return $"{GetType().Name} color:{color} size:{size}";
-    }
-}
-
-class Mammalia : Animal, IAnimal {
-    protected int numberBabies;
-
-    public Mammalia(Color color, Size size, int numberBabies)
-        : base(color, size) {
-        this.numberBabies = numberBabies;
+    enum Size
+    {
+        SMALL, MEDIUM, LARGE, HUGE
     }
 
-    public virtual void Sound() {
-        Console.WriteLine("Mammalia sound");
+    interface IAnimal
+    {
+        void Sound();
+        void Eat();
     }
 
-    public virtual void Eat() {
-        Console.WriteLine("Mammalia eat");
+    interface IMammalia
+    {
+        void Run();
     }
 
-    public void Run() {
-        Console.WriteLine("Mammalia run");
-    }
-}
-
-class Dog : Mammalia {
-    protected bool fierce;
-
-    public Dog(Color color, Size size, int numberBabies, bool fierce)
-        : base(color, size, numberBabies) {
-        this.fierce = fierce;
+    interface IDog
+    {
+        void Bark();
+        void Bite();
     }
 
-    public override void Sound() {
-        Console.WriteLine("Dog sound");
+    interface IAves
+    {
+        void Fly();
     }
 
-    public override void Eat() {
-        Console.WriteLine("Dog eat");
+    interface IFish
+    {
+        void Swimming();
     }
 
-    public override string ToString() {
-        return $"Dog color:{color} size:{size} fierce:{fierce}";
-    }
-}
+    abstract class Animal : IAnimal
+    {
+        protected Color color;
+        protected Size size;
 
-class Bird : Animal, IAnimal {
-    protected int egg;
+        public Animal(Color color, Size size)
+        {
+            this.color = color;
+            this.size = size;
+        }
 
-    public Bird(Color color, Size size, int egg)
-        : base(color, size) {
-        this.egg = egg;
-    }
+        public virtual void Sound()
+        {
+            Console.WriteLine("animal");
+        }
 
-    public void Sound() {
-        Console.WriteLine("Bird sound");
-    }
+        public virtual void Eat()
+        {
+            Console.WriteLine("eat animal");
+        }
 
-    public void Eat() {
-        Console.WriteLine("Bird eat");
-    }
-}
-
-class Fish : Animal, IAnimal {
-    protected string waterGroup;
-
-    public Fish(Color color, Size size, string waterGroup)
-        : base(color, size) {
-        this.waterGroup = waterGroup;
+        public virtual string ToStringAnimal()
+        {
+            return "Animal";
+        }
     }
 
-    public void Sound() {
-        Console.WriteLine("Fish sound");
+    class Mammalia : Animal, IMammalia
+    {
+        protected int numberBabies;
+
+        public Mammalia(Color color, Size size, int numberBabies)
+            : base(color, size)
+        {
+            this.numberBabies = numberBabies;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Mammalia sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Mammalia eat");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Mammalia Run");
+        }
     }
 
-    public void Eat() {
-        Console.WriteLine("Fish eat");
+    class Dog : Mammalia, IDog
+    {
+        protected bool fierce;
+
+        public Dog(Color color, Size size, int numberBabies, bool fierce)
+            : base(color, size, numberBabies)
+        {
+            this.fierce = fierce;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("sound Dog");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Dog eat");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Hong!");
+        }
+
+        public void Bite()
+        {
+            Console.WriteLine("kad!!");
+        }
+
+        public override string ToStringAnimal()
+        {
+            return "Dog";
+        }
     }
-}
 
-class Program {
-    static void Main() {
-        IAnimal a1 = new Dog(Color.BROWN, Size.MEDIUM, 5, true);
-        IAnimal a2 = new Bird(Color.WHITE, Size.SMALL, 2);
-        IAnimal a3 = new Fish(Color.GREY, Size.LARGE, "Fresh");
+    class ThaiRidgeBack : Dog
+    {
+        public ThaiRidgeBack(Color color, Size size, int numberBabies, bool fierce)
+            : base(color, size, numberBabies, fierce) { }
 
-        a1.Sound();
-        a2.Sound();
-        a3.Sound();
+        public override void Sound()
+        {
+            Console.WriteLine("Dog thai hong");
+        }
 
-        Console.WriteLine(a1);
+        public override void Eat()
+        {
+            Console.WriteLine("Dog thai eat");
+        }
+    }
+
+    class Aves : Animal, IAves
+    {
+        public Aves(Color color, Size size) : base(color, size) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Aves sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Aves eat");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Aves Fly");
+        }
+    }
+
+    class Bird : Aves
+    {
+        protected int egg;
+
+        public Bird(Color color, Size size, int egg)
+            : base(color, size)
+        {
+            this.egg = egg;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Bird sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Bird eat");
+        }
+
+        public override string ToStringAnimal()
+        {
+            return "Bird";
+        }
+    }
+
+    class HummingBird : Bird
+    {
+        public HummingBird(Color color, Size size, int egg)
+            : base(color, size, egg) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Humming sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("humming eat");
+        }
+    }
+
+    class Osteicthyes : Animal, IFish
+    {
+        public Osteicthyes(Color color, Size size) : base(color, size) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("sound Os");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("eat Os");
+        }
+
+        public void Swimming()
+        {
+            Console.WriteLine("os swimming");
+        }
+    }
+
+    class Fish : Osteicthyes
+    {
+        protected string waterGroup;
+
+        public Fish(Color color, Size size, string waterGroup)
+            : base(color, size)
+        {
+            this.waterGroup = waterGroup;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("sound Fish");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("eat Fish");
+        }
+
+        public override string ToStringAnimal()
+        {
+            return "Fish";
+        }
+    }
+
+    class AngleFish : Fish
+    {
+        protected string location;
+
+        public AngleFish(Color color, Size size, string waterGroup, string location)
+            : base(color, size, waterGroup)
+        {
+            this.location = location;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("AngleFish Sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eat AngleFish");
+        }
+
+        public override string ToStringAnimal()
+        {
+            return "AngleFish";
+        }
     }
 }
